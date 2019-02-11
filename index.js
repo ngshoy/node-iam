deaa1947d2bcde42af18822020d23ae720a5ce41
Jack Zhang <ngshoy@hotmail.com>
2019-02-10 22:18:09 -0500

    add /login endpoint details to assign jwt for logged in user

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const expressjwt = require('express-jwt');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+const users = [{
+    id: 1,
+    username: 'admin',
+    password: 'admin'
+  },
+  {
+    id: 2,
+    username: 'guest',
+    password: 'guest'
+  },
+]
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/health', (req, res) => {
   const localTime = (new Date()).toLocaleTimeString();
@@ -13,11 +28,38 @@ app.get('/health', (req, res) => {
     .send(`Server time is ${localTime}`);
 });
 
+app.get('/secret', (req, res) => {
+
+})
+
 app.post('/login', (req, res) => {
-  const user = req.body.username;
+  if (!req.body.username || !req.body.password) {
+    res.status(400);
+    res.send('You need a username and password');
+    return;
+  }
+
+  const user = users.find(u => {
+    return u.username === req.body.username && u.password === req.body.password;
+  });
+
+  if (!user) {
+    res.status(401);
+    res.send('User not found');
+    return;
+  }
+
+  const token = jwt.sign({
+    sub: user.id,
+    username: user.username
+  }, 'mysupersecretekey', {
+    expiresIn: '3 hours'
+  });
   res
     .status(200)
-    .send(`You logged in with ${user}`);
+    .send({
+      access_token: token
+    });
 });
 
 app.get('*', (req, res) => {
